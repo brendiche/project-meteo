3e146ff4acc3b9f0cee44806b4806810863cf6f6
bgouin <bgouin@astek.fr>
2016-06-17 15:22:36 +0200

    init

---
+++ src/app/core/core.module.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+  angular
+    .module('core', [
+    	'ngAnimate',
+    	'ngCookies',
+    	'ngTouch',
+    	'ngSanitize',
+    	'ngMessages',
+    	'ngAria',
+    	'ui.router',
+    	'ui.bootstrap',
+    	'toastr'
+    	]);
+})();
