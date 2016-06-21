b662775f31fe0f56caf6535156259173ea57e8cd
bgouin <bgouin@astek.fr>
2016-06-21 16:42:33 +0200

    finish nav bar

--- src/app/technical/marker/maker.run.js
+++ src/app/technical/marker/maker.run.js
@@ -6,10 +6,11 @@
     .run(initMarker);
 
   /** @ngInject */
-  function initMarker(markerService,$log) {
+  function initMarker(markerService,$log,$rootScope) {
 
     $log.debug('Init marker start');
     markerService.init().then(function(markers){
+      $rootScope.$broadcast('markerReady');
     	$log.debug('Init marker end : ' ,markers);
     })
   }
--- src/app/technical/marker/maker.service.js
+++ src/app/technical/marker/maker.service.js
@@ -33,47 +33,28 @@
                         $state.go('app.main.city',{"city" : cityToGo});
                     },
                     show : angular.equals(city,"Paris"),
-                    // weather : meteoService.getWeatherForCity(CONFIG.APP.CITIES[city]),
-                    weather :{},
+                    weather : meteoService.getWeatherForCity(CONFIG.APP.CITIES[city]),
                     options: {
                         draggable: false
                     }
                   };
                 markers.markers.push(marker);
             }
-            if(!localStorage.getItem("weathers")){
-                $q.all(markers.markers.map(function(marker){return marker.weather;})).then(function(weathers){
-                    console.log(weathers);
-                    localStorage.setItem("weathers",JSON.stringify(weathers));
-                    for (var i = 0; i < weathers.length; i++) {
-                        if(CONFIG.APP.WEATHER[weathers[i].weather[0].main]){
-                            weathers[i].weather[0].main = CONFIG.APP.WEATHER[weathers[i].weather[0].main];
-                        }else{
-                            weathers[i].weather[0].main = CONFIG.APP.WEATHER["UNKNOWN"];
-                        }
-                        weathers[i].main.temp = Math.round((weathers[i].main.temp-273)*10)/10+' °C';
-                        markers.markers[i].weather = weathers[i];
+            $q.all(markers.markers.map(function(marker){return marker.weather;})).then(function(weathers){
+                console.log(weathers);
+                localStorage.setItem("weathers",JSON.stringify(weathers));
+                for (var i = 0; i < weathers.length; i++) {
+                    if(CONFIG.APP.WEATHER[weathers[i].weather[0].main]){
+                        weathers[i].weather[0].main = CONFIG.APP.WEATHER[weathers[i].weather[0].main];
+                    }else{
+                        weathers[i].weather[0].main = CONFIG.APP.WEATHER["UNKNOWN"];
                     }
-                });
-
-            }else{
-                var items =  JSON.parse(localStorage.getItem("weathers"));
-                for (var i = 0; i < items.length; i++) {
-                    if(items[i].weather){
-                        if(CONFIG.APP.WEATHER[items[i].weather[0].main]){
-                            items[i].weather[0].main = CONFIG.APP.WEATHER[items[i].weather[0].main];
-                        }else{
-                            items[i].weather[0].main = CONFIG.APP.WEATHER["UNKNOWN"];
-                        }
-                        items[i].main.temp = Math.round((items[i].main.temp-273)*10)/10+' °C';
-                        markers.markers[i].weather = items[i];
-                    }
+                    weathers[i].main.temp = Math.round((weathers[i].main.temp-273)*10)/10+' °C';
+                    markers.markers[i].weather = weathers[i];
                 }
-            }
+                deferred.resolve(markers.markers);
+            });
 
-
-
-            deferred.resolve(markers.markers);
 
             return deferred.promise;
         }
